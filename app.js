87fd3e9c1e88f21b0c79232106595371a2a2afdb
Jorge <newdarkjg@gmail.com>
2024-01-26 13:21:23 +0100

    ejercicio terminado

--- app.js
+++ app.js
@@ -1,57 +1,76 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
-const e = require("express");
 const express = require("express");
 const app = express();
 const url = "https://es.wikipedia.org/wiki/Categor%C3%ADa:M%C3%BAsicos_de_rap";
-const url2 = "https://es.wikipedia.org/wiki";
+const url2 = "https://es.wikipedia.org";
 
-app.get("/",  (req, res) => {
+app.get("/", async (req, res) => {
     let enlaces = [];
-    axios.get(url).then((response) => {
-        if (response.status == 200) {
-            const html = response.data;
-            const $ = cheerio.load(html)
-            $('#mw-pages a').each((index, element) => {
-                let enlace = $(element).attr("href")
-                let enlace2 = url2 + enlace;
+    try {
+        axios.get(url).then(async (response) => {
+            if (response.status == 200) {
+                const html = response.data;
+                const $ = cheerio.load(html)
+                $('#mw-pages a').each((index, element) => {
+                    let enlace = $(element).attr("href")
+                    let enlace2 = url2 + enlace;
+                    enlaces.push((enlace2));
+                })
+                const datosCompletos = []
 
-                enlaces.push(encodeURIComponent(enlace2));
-            })
-           axios.all(enlaces.map((endpoint) => axios.get(endpoint))).then((data) =>{ if (response.status == 200){console.log(data)}})
-        }
-        
-    })
-    
-  
-    
+                for (const link of enlaces) {
+
+                    let data = {
+                        imagenes: [],
+                        pes: []
+                    }
+
+                    const respuesta = await axios.get(link)
+                    const html = respuesta.data
+                    const $ = cheerio.load(html)
+
+                    data.titulo = $('title').text()
+
+                    $("img").each((index, element) => {
+                        const src = $(element).attr("src")
+                        data.imagenes.push(src)
+                    })
+
+                    $("p").each((index, element) => {
+                        const parf = $(element).text()
+                        data.pes.push(parf)
+                    })
+                    datosCompletos.push(data)
+                    console.log(data)
+                    console.log(datosCompletos.length)
+
+                }
+
+                res.send(`
+                <ul>
+                  ${datosCompletos.map(dato => `<li><h2>${dato.titulo}</h2>
+                  imagenes: ${dato.imagenes.map(img => `<p>${img}</p>`).join('')} 
+                  párrafos: ${dato.pes.map(pes => `<p>${pes}</p>`).join('')} 
+                  </li>`).join('')}
+                </ul>
+               `
+                )
+            }
+
+
+
+
+
+
+        })
+
+    } catch {
+
+    }
+
 })
-    app.listen(3001, () => {
-        console.log("Esta escuchando en http://127.0.0.1:3001")
-    })
+app.listen(3001, () => {
+    console.log("Esta escuchando en http://127.0.0.1:3001")
+})
 
-/*
-             let dato = new Object();
-                                dato = {
-                                    titulo: "",
-                                    pes: "",
-                                    img: ""
-                                }
-                                if (res.status == 200) {
-                                    const html2 = res.data;
-                                    const che = cheerio.load(html2)
-            
-                                    let pageTitle = che("title").text()
-                                    dato.titulo = pageTitle;
-            
-                                    che("p").each((index, element) => {
-                                        const pe = che(element)
-                                        dato.pes = pe
-                                    })
-                                    che("img").each((index, element) => {
-                                        const img = che(element).attr("src")
-                                        dato.img = img;
-                                    })
-                                    console.log(dato)
-                                }*/
-                        
